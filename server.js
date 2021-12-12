a9e989082efbd416bd8528c6b7f26b8e87c98c7d
gsdkira <larreakelly@gmail.com>
2021-12-12 16:37:54 -0500

    required routes in server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
+const itemRoutes = require('./app/routes/item_routes')
+const feedbackRoutes = require('./app/routes/feedback_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -67,6 +69,8 @@ app.use(requestLogger)
 // register route files
 app.use(exampleRoutes)
 app.use(userRoutes)
+app.use(itemRoutes)
+app.use(feedbackRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
